779e130b4416cd68927d55eb39b84283a0bde192
Abhinav Thapa <abhinavthapa1998@gmail.com>
2023-12-30 14:00:11 +0530

    added auth and metadata

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,6 +4,8 @@ import { z } from 'zod';
 import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
 
 export type State = {
   errors?: {
@@ -39,13 +41,13 @@ export async function createInvoice(prevState: State, formData: FormData) {
     status: formData.get('status'),
   });
   // If form validation fails, return errors early. Otherwise, continue.
-  if (!validatedFields.success){
+  if (!validatedFields.success) {
     return {
       errors: validatedFields.error.flatten().fieldErrors,
-      message: "Missing Fields. Failed to Create Invoice",
-    }
+      message: 'Missing Fields. Failed to Create Invoice',
+    };
   }
-  const {customerId, amount, status} = validatedFields.data;
+  const { customerId, amount, status } = validatedFields.data;
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split('T')[0];
 
@@ -91,4 +93,23 @@ export async function deleteInvoice(id: string) {
     return { message: 'Database Error: Failed to Update Invoice.' };
   }
   revalidatePath('/dashboard/invoices');
+}
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials';
+        default:
+          return 'Something went wrong';
+      }
+    }
+    throw error;
+  }
 }
